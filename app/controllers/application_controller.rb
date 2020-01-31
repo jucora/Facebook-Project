5d5be39e978437a701fd0693992a1888e170cebd
jucora <artjulius@hotmail.com>
2020-01-31 10:38:00 -0500

    Devise authentication working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_devise_params, if: :devise_controller?
+
+	def configure_devise_params
+		devise_parameter_sanitizer.permit(:sign_up) do |user|
+			user.permit(:name, :email, :password, :password_confirmation)
+		end
+	end
 end
