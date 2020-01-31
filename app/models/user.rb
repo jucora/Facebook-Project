110195dd3ab55092dc37673b339c72b33cabbf4c
jucora <artjulius@hotmail.com>
2020-01-31 15:15:22 -0500

    rspec and capybara tests for user and session

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, length: { minimum: 6 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 } 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name:'john', email:'john@mail.com', password: 'password', password_confirmation: 'password') }
+  let(:another_user) { User.create(name:'john', email:'john@mail.com', password: 'password', password_confirmation: 'password') }
+
+  describe '#name' do
+  	it 'should have a name' do
+  		user.name = ''
+  		user.valid?
+  		expect(user.errors[:name]).to include("can't be blank")
+  	end
+
+  	it 'should have at least 2 characters' do
+  		user.name = 'a'
+  		user.valid?
+  		expect(user.errors[:name]).to include("is too short (minimum is 2 characters)")
+  	end
+
+  	it 'should have a maximum of 50 characters' do
+  		user.name = 'a' * 51
+  		user.valid?
+  		expect(user.errors[:name]).to include("is too long (maximum is 50 characters)")
+  	end
+  end
+
+  describe '#email' do
+  	it 'should have an email' do
+  		user.email = ''
+  		user.valid?
+  		expect(user.errors[:email]).to include("can't be blank")
+  	end
+
+  	it 'should have a valid format' do
+  		user.email = 'user'
+  		user.valid?
+  		expect(user.errors[:email]).to include('is invalid')
+  	end
+
+  	it 'should be unique' do
+  		another_user.valid?
+  		expect(user.errors[:email]).to include('has already been taken')
+  	end
+  end
+
+  describe '#password' do
+  	it 'should have a minimum of 6 characters' do
+  		user.password = 'pass'
+  		user.valid?
+  		expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+  	end
+
+  	it 'should have a password' do
+  		user.password = ''
+  		user.valid?
+  		expect(user.errors[:password]).to include("can't be blank")
+  	end
+  end
+
+  describe '#password confirmation' do
+  	it 'should match the password' do
+  		user.password = 'password'
+  		user.password_confirmation = 'pass'
+  		user.valid?
+  		expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+  	end
+  end
 end
