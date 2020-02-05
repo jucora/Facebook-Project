6b5fb2fab5cd955faf7874ce54587ef1ebcca28f
hugopassos <hugo.passos@hotmail.com>
2020-02-05 12:56:06 -0300

    Finished Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,26 @@
 class LikesController < ApplicationController
 	before_action :find_post, only: [:create, :destroy]
-	
+
 	def create
-		@post = Post.find(params[:post_id])
-		@like = @post.likes.new(user_id: current_user.id)
-		@like.save
+		@post.likes.create(user_id: current_user.id) if !like_exists?
 		redirect_to post_path(@post)
 	end
-	
+
 	def destroy
-		@post = Post.find_by(id: params[:post_id])
-		@like = @post.likes.find_by(id: params[:id])
-		@like.destroy
+		if like_exists?
+			@like = @post.likes.find(params[:id])
+			@like.destroy
+		end
+		redirect_to post_path(@post)
 	end
 
+	private
+
+	def find_post
+		@post = Post.find(params[:post_id])
+	end
+
+	def like_exists?
+		Like.where(user_id: current_user.id, post_id: params[:post_id]).any?
+	end
 end
