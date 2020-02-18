3ed37b1d8d20b83dce61dad49106de4cfd84060e
jucora <artjulius@hotmail.com>
2020-02-18 12:10:58 -0500

    added size functionality for user_gravatar, improved comments, post show and post edit styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-	def user_gravatar(user, options = { size: 50 })
+	def user_gravatar(user, options = { size: 100 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: 'gravatar')
 	end
 end
